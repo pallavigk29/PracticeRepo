1ed2d6aaabe9051d663a3f4ad6c7bc9bae14e14a
hitesth <hiteshgehlotit@gmail.com>
2023-10-12 11:39:06 +0530

    login page code done

--- Mock1/src/main/java/com/mock1/pageLayer/LoginPage.java
+++ Mock1/src/main/java/com/mock1/pageLayer/LoginPage.java
@@ -1,5 +1,11 @@
 package com.mock1.pageLayer;
 
 public class LoginPage {
+	
+	public void loginfunctionality()
+	{
+		
+		System.out.println("login funstionality test");
+	}
 
 }
--- Mock1/src/test/java/com/mock1/testLayer/LoginTest.java
+++ Mock1/src/test/java/com/mock1/testLayer/LoginTest.java
@@ -1,5 +1,14 @@
 package com.mock1.testLayer;
 
+import org.testng.annotations.Test;
+
 public class LoginTest {
+	
+	@Test
+	public void verifiedloginwithvaliddata()
+	{
+		
+		System.out.println("login verified with valid credential");
+	}
 
 }
